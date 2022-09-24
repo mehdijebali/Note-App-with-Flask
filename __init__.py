4a7cfe29afe05c6775f0002cef93bc31ec455c9b
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-24 17:42:38 +0100

    fix edit bug

--- __init__.py
+++ __init__.py
@@ -116,11 +116,38 @@ def create_app(test_config=None):
 
         return render_template('note_create.html')
     
-    @app.route('/notes/<note_id>/edit')
+    @app.route('/notes/<note_id>/edit', methods=('GET', 'POST', 'PUT', 'PATCH'))
     @require_login
     def note_update(note_id):
         note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+        if request.method in ['POST', 'PATCH', 'PUT']:
+            title = request.form['title']
+            body = request.form['body']
+            error = None
+
+            if not title:
+                error = 'Title is required.'
+
+            if not error:
+                note.title = title
+                note.body = body
+                db.session.add(note)
+                db.session.commit()
+                flash(f"Successfully updated note: '{title}'", 'success')
+                return redirect(url_for('note_index'))
+
+            flash(error, 'error')
         return render_template('note_update.html', note=note)
+    
+    @app.route('/notes/<note_id>/delete', methods=('GET', 'DELETE'))
+    @require_login
+    def note_delete(note_id):
+        note = Note.query.filter_by(user_id=g.user.id, id=note_id).first_or_404()
+        db.session.delete(note)
+        db.session.commit()
+        flash(f"Successfully deleted note: '{note.title}'", 'success')
+        return redirect(url_for('note_index'))
+
 
     @app.route('/log_out', methods=('GET', 'DELETE'))
     def log_out():
