5b30a3c54330d4b16fcc5013211be424135ecb61
mehdijebali <mehdi.benhedi.jebali@gmail.com>
2022-09-24 12:24:15 +0100

    add app config

--- __init__.py
+++ __init__.py
@@ -6,26 +6,18 @@ from werkzeug.security import generate_password_hash
 
 def create_app(test_config=None):
     # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
+    app = Flask(__name__)
     app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
+        SECRET_KEY=os.environ.get('SECRET_KEY', default='dev'),
     )
 
     if test_config is None:
-        # load the instance config, if it exists, when not testing
         app.config.from_pyfile('config.py', silent=True)
     else:
-        # load the test config if passed in
         app.config.from_mapping(test_config)
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-    from models import db, User
+    from .models import db, User
 
     db.init_app(app)
     migrate = Migrate(app, db)
